a68a4d12ce3d228ce38e9e60243ed6f84ffca1cb
Santi_Vilanova <santi@playmodes.com>
2024-11-26 09:28:02 +0100

    updated addon_config

--- addon_config.mk
+++ addon_config.mk
@@ -6,30 +6,17 @@ meta:
     ADDON_URL = http://github.com/yourusername/ofxOceanodeOnlineRadio
 
 common:
-    # dependencies with other addons
     ADDON_DEPENDENCIES = ofxOceanode
-
-    # source files - include both .cpp and .h files
     ADDON_SOURCES = src/radioStationVLC.cpp
     ADDON_SOURCES += src/radioStationVLC.h
     ADDON_SOURCES += src/ofxOceanodeOnlineRadio.h
-    
-    # include search paths
     ADDON_INCLUDES = src
 
 osx:
-    # VLC paths
-    ADDON_INCLUDES += "/Applications/VLC.app/Contents/MacOS/include"
+    # Include paths
+    ADDON_INCLUDES += /Applications/VLC.app/Contents/MacOS/include
     
-    # Compiler flags
-    ADDON_CFLAGS = -DVLC_PLUGIN_PATH='\"/Applications/VLC.app/Contents/MacOS/plugins\"'
-    ADDON_CPPFLAGS = -DVLC_PLUGIN_PATH='\"/Applications/VLC.app/Contents/MacOS/plugins\"'
-    
-    # Linker flags
-    ADDON_LDFLAGS = -L"/Applications/VLC.app/Contents/MacOS/lib"
+    # Library paths with proper rpath settings
+    ADDON_LDFLAGS = -L/Applications/VLC.app/Contents/MacOS/lib
     ADDON_LDFLAGS += -lvlc -lvlccore
-    ADDON_LDFLAGS += -Wl,-rpath,"/Applications/VLC.app/Contents/MacOS/lib"
-    
-    # Libraries
-    ADDON_LIBS = "/Applications/VLC.app/Contents/MacOS/lib/libvlc.dylib"
-    ADDON_LIBS += "/Applications/VLC.app/Contents/MacOS/lib/libvlccore.dylib"
+    ADDON_LDFLAGS += -Wl,-rpath,/Applications/VLC.app/Contents/MacOS/lib
